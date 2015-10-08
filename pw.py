df431341658e6c0320859ea604fb3bcb6a993286
Steve Wollkind <wollkind@cargurus.com>
2015-10-08 11:13:51 -0400

    add conference to team activity

--- pw.py
+++ pw.py
@@ -296,23 +296,24 @@ def get_team_activity(league,con):
                 team_id = m.group(1)
                 tds = [item.text for item in row.find_all('td')]
                 last_seen = tds[5]
+                conf = tds[0]
                 if last_seen == '':
-                    next
+                    continue
                 date_object = datetime.datetime.strptime(last_seen, '%m/%d/%y')
-                activities.append([league.value, team_id, date_object, team_name, division])
+                activities.append([league.value, team_id, date_object, team_name, division, conf])
                 log("{} {}".format(team_id, last_seen))
 
-        query = "insert into team_activity (league_id, team_id, last_seen, team_name, division) VALUES (%s, %s, %s, %s, %s)"
+        query = "insert into team_activity (league_id, team_id, last_seen, team_name, division, conf) VALUES (%s, %s, %s, %s, %s, %s)"
         cur.executemany(query, activities)
 
 
 if __name__ == '__main__':
 
-    #con = mysql.connector.connect(host='devbox-me', user='oleepoth', password='urcify', database='pw', port='3316')
-    con = mysql.connector.connect(host='localhost', user='root', password='', database='pw')
+    con = mysql.connector.connect(host='devbox-me', user='oleepoth', password='urcify', database='pw', port='3316')
+    #con = mysql.connector.connect(host='localhost', user='root', password='', database='pw')
     con.autocommit = True
 
     cur = con.cursor()
     cur.execute("SET @@session.sql_mode= ''")
 
-    get_league_date(League.mays)
+    get_team_activity(League.foxx, con)
