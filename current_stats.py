d3f41150fbd9810c6eaab0a3a92dff49882c0a14
wollkind <steve@cargurus.com>
2018-11-18 01:39:56 -0500

    Various fixes

--- current_stats.py
+++ current_stats.py
@@ -18,9 +18,11 @@ if __name__ == '__main__':
     for league in leagues:
         league_count += 1
 
-        year = get_league_date(league).year - 1
+        year = get_league_date(league).year
 
-        log("Starting {} {} ({} of {})".format(league.name, year, year_count, len(years)))
+
+        log("Starting {} {} ({} of {})".format(league.name, year))
+
         get_all_stats_for_league_year(league, year, teams, levels, types, con)
 
 
